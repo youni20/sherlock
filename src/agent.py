c1a43c77f0c6e2ae8fe761e082bc9fc8444a2113
Younus <younusmashoor@gmail.com>
2026-05-21 22:15:21 +0200

    chore: type casted variables model and agent for cleaner code and safety

--- src/agent.py
+++ src/agent.py
@@ -1,10 +1,12 @@
 from langchain.agents import create_agent
+from langchain_core.language_models import BaseChatModel
+from langgraph.pregel.main import Runnable
 from prompts import SYSTEM_PROMPT
 from langchain_ollama import ChatOllama
 
-model = ChatOllama(model="gemma3", temperature=0)
+model: BaseChatModel = ChatOllama(model="gemma3", temperature=0)
 
-agent = create_agent(
+agent: Runnable = create_agent(
     model=model,
     system_prompt=SYSTEM_PROMPT
 )
