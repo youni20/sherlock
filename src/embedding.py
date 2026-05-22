23eac12554a087e14260b29c2521fad07a9d8c70
Younus <younusmashoor@gmail.com>
2026-05-22 14:43:22 +0200

    feat: split the store vector function into 2 for easier method calling and flow of the program

--- src/embedding.py
+++ src/embedding.py
@@ -4,11 +4,27 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 
 #  Initialise the embedding model
-embedding_model = OllamaEmbeddings(
+embedding_model: OllamaEmbeddings = OllamaEmbeddings(
     model="embeddinggemma:latest",
     dimensions=768
 )
 
+
+#  Split store vector into 2 functions one to store it and one to query it 
+def build_vector_store(chunks: list[str]) -> InMemoryVectorStore:
+    return InMemoryVectorStore.from_texts(chunks, embedding=embedding_model)
+
+def retrive_context(vector_store: InMemoryVectorStore, question: str) -> str:
+    retriever: VectorStoreRetriever = vector_store.as_retriever()
+    retrieved_documents: list[Document] = retriever.invoke(question)
+    chunks: list[str] = []
+    for doc in retrieved_documents:
+        chunks.append(doc.page_content)
+
+    return "\n\n".join(chunks)
+    
+
+'''
 #  Define method to store the chunks (for now in memory ill make a dir for it later)
 def store_vector(text: list[str], question: str):
     vector_store = InMemoryVectorStore.from_texts(text, embedding=embedding_model)
@@ -16,4 +32,4 @@ def store_vector(text: list[str], question: str):
     retrived_documents: list[Document] = retriever.invoke(question)
 
     return retrived_documents[0].page_content
-    
+'''
