a04ea54db83ab9825f2566098960aa7830168574
Younus <younusmashoor@gmail.com>
2026-05-22 15:17:21 +0200

    feat: added a nested for loop to iterate over all files in the case_files directory so that not just one file is being tested on

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ from embedding import build_vector_store, retrive_context
 from chunk import split_text
 from ingest import load_document, DATA_COLLECTION
 from dotenv import load_dotenv
+import os
 
 
 def main() -> None:
     load_dotenv()
-    text = load_document("test.txt")
-    chunks_list = split_text(document=text)
-    vector_store = build_vector_store(chunks=chunks_list)
+    for root, dir, files in os.walk(DATA_COLLECTION):
+        for file in files:
+            text = load_document(file)
+            chunks_list = split_text(document=text)
+            vector_store = build_vector_store(chunks=chunks_list)
     
     # print(f"Character count: {len(text)}")
 
@@ -17,7 +20,6 @@ def main() -> None:
     docs: str = retrive_context(vector_store=vector_store, question=question)
     answer: str = ask_question(question, docs)
     print(answer)
-
 
     
 if __name__ == "__main__":
