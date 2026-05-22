ee067df7ef895e12f2fa4e9d1e11e6058131b383
Younus <younusmashoor@gmail.com>
2026-05-22 14:46:24 +0200

    feat: updated main to use new embedding functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from agent import ask_question
-from embedding import embedding_model, store_vector
+from embedding import build_vector_store, retrive_context
 from chunk import split_text
 from ingest import load_document, DATA_COLLECTION
 from dotenv import load_dotenv
@@ -8,12 +8,13 @@ from dotenv import load_dotenv
 def main() -> None:
     load_dotenv()
     text = load_document("test.txt")
-    chunks_list = split_text(text)
+    chunks_list = split_text(document=text)
+    vector_store = build_vector_store(chunks=chunks_list)
     
     # print(f"Character count: {len(text)}")
 
     question: str = input("Question: ")
-    docs: str = store_vector(text=chunks_list, question=question)
+    docs: str = retrive_context(vector_store=vector_store, question=question)
     answer: str = ask_question(question, docs)
     print(answer)
 
