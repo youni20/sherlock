a21683bca71aa8b73e70a1a220322181267bee23
Younus <younusmashoor@gmail.com>
2026-05-25 18:40:03 +0200

    feat: started working on fastapi backend made basic home page setup with main.py as the starting point and added my notes markdown file to the .gitignore

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from doctest import debug
+
 from system import bootstrap_knowledge_base, run_cli
+from fastapi import FastAPI
+import uvicorn
+
+app: FastAPI = FastAPI()
+
+@app.get("/")
+def home() -> str:
+    return "Home Page"
+
 
 if __name__ == "__main__":
-    bootstrap_knowledge_base()
-    run_cli()
+    uvicorn.run(app='main:app', port=8080, reload=True)
+    # bootstrap_knowledge_base()
+    # run_cli()
